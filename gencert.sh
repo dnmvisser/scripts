56feb5fecf3b570b7d30619646e9fdb6bd2ab126
Dick Visser <dick.visser@geant.org>
2020-07-30 21:07:00 +0200

    final wokr, docs

--- gencert.sh
+++ gencert.sh
@@ -1,24 +1,51 @@
 #!/usr/bin/env bash
 #
 # Script to generate a private key and corresponding Certificate
-# Signing Request, or self-signed certificate.
+# Signing Request, or create a self-signed certificate.
 # For increased security, the private key is not written to disk.
+# 
+# Can also use an existing private key, by piping the PEM value to
+# the script.
 #
-# First items are defaults
+# The first item in each variable is the default
 SIGN_TYPES=(CA-signed Self-signed)
 KEY_TYPES=(ECC RSA)
 EC_CURVES=(prime256v1 secp384r1)
 RSA_SIZES=(2048 3072 4096)
 
-
+# Help message if not enough parameters are provided
 if [ $# -lt 1 ]; then
-  echo "Usage: ${BASH_SOURCE} cn <san_1 san_2 san_3 san_4 ... san_n>
+  me=`basename ${BASH_SOURCE}`
+  echo "Usage: $me cn <san_1 san_2 san_3 san_4 ... san_n>
 The first argument is cn (Common Name).
-Optional further arguments are treated as Subject Altenative Names."
+Optional further arguments are treated as Subject Altenative Names.
+
+It is also possible to reuse an existing private key by providing it
+to STDIN.
+
+Examples:
+
+# Generate a new CSR and key for a hostname and a few SANs
+$me www.uni.edu uni.edu old_uni.edu
+
+# The same, but reuse an existing private key
+cat privkey.pem | $me www.uni.edu uni.old other.tld
+
+# Similar, but fetch the private key from an ansible vault
+ansible-vault view privkey.vault | $me www.uni.edu uni.old other.tld
+
+# Similar, but fetch the private key from a variable in an ansible vaulted YAML file
+ansible-vault view vault.yml | yq -r .privkey | $me www.uni.edu uni.old other.tld
+
+# Generate a self signed key pair for a SAML Service Provider
+$me 'My Fancy New Service'
+
+"
   exit 1
 fi
 
-# mimics bash' select
+
+# Supporting function, to mimics bash' "select"
 function selectWithDefault() {
   local item i=0 numItems=$#
   # Print numbered menu items, based on the arguments passed.
@@ -47,7 +74,9 @@ function selectWithDefault() {
 }
 
 
-# Test to see if we have a valid private key in stdin
+
+
+# Check if we received a valid private key in STDIN
 if ! test -t 0 ; then
   INPUT="$(< /dev/stdin)"
   if echo "$INPUT" | openssl pkey -noout; then
@@ -58,39 +87,42 @@ if ! test -t 0 ; then
 fi
 
 
-
-echo "Select generation type"
+# Select how the certificate should be signed
+# Defaults to CA signed, which results in a CSR.
+# It self-signed it picked, then the key type will be RSA,
+# as this is much more common, for instance for SAML
+# certificates
+echo "Select signing type"
 SIGN_TYPE=$(selectWithDefault "${SIGN_TYPES[@]}")
-# For self-signed RSA is better default
 if [ $SIGN_TYPE = "Self-signed" ]; then
   KEY_TYPES=(RSA ECC)
 fi
 
 
-# FIXME only do this when we don't have a privaite key from stdin
-echo "Select key type"
-KEY_TYPE=$(selectWithDefault "${KEY_TYPES[@]}")
-
-if [ "$KEY_TYPE" = "ECC" ]; then
-  echo "Select curve"
-  EC_CURVE=$(selectWithDefault "${EC_CURVES[@]}")
-  EC_PARAMS=$(mktemp)
-  openssl ecparam -name ${EC_CURVE} -out ${EC_PARAMS}
-  KEY_SPEC="ec:${EC_PARAMS}"
-elif [ "$KEY_TYPE" = "RSA" ]; then
-  echo "Select RSA size"
-  RSA_SIZE=$(selectWithDefault "${RSA_SIZES[@]}")
-  KEY_SPEC="rsa:${RSA_SIZE}"
-else
-  echo "Not implemented yet"
-fi
-
-
-#
+# Key generation flags
 if [ -z "$PRIVATE_KEY" ]; then
-  KEY_SRC="-newkey ${KEY_SPEC} -keyout /dev/stdout 2>/dev/null"
+  # This is only needed when we do NOT have a valid private key from STDIN
+  echo "Select key type"
+  KEY_TYPE=$(selectWithDefault "${KEY_TYPES[@]}")
+
+  if [ "$KEY_TYPE" = "ECC" ]; then
+    # Select ECC curve
+    echo "Select curve"
+    EC_CURVE=$(selectWithDefault "${EC_CURVES[@]}")
+    EC_PARAMS=$(mktemp)
+    openssl ecparam -name ${EC_CURVE} -out ${EC_PARAMS}
+    KEY_SPEC="-newkey ec:${EC_PARAMS} -keyout /dev/stdout 2>/dev/null"
+  elif [ "$KEY_TYPE" = "RSA" ]; then
+    # Select RSA key size
+    echo "Select RSA size"
+    RSA_SIZE=$(selectWithDefault "${RSA_SIZES[@]}")
+    KEY_SPEC="-newkey rsa:${RSA_SIZE} -keyout /dev/stdout 2>/dev/null"
+  else
+    echo "Not implemented yet"
+  fi
 else
-  KEY_SRC="-new -key <(echo -n \"${PRIVATE_KEY}\")"
+  # We use the private key from STDIN
+  KEY_SPEC="-new -key <(echo -n \"${PRIVATE_KEY}\")"
 fi
 
 
@@ -98,12 +130,12 @@ fi
 if [ $SIGN_TYPE = "CA-signed" ]; then
   if [ $# -eq 1 ]; then
     # One argument => simple scenario
-    eval "openssl req -nodes -subj /CN=${1}/ $KEY_SRC"
+    eval "openssl req -nodes -subj /CN=${1}/ $KEY_SPEC"
   else
     # More than one argument => first arg is CN, the rest is the list of SANs
     s="subjectAltName="
     for san in `echo $@`; do s="${s}DNS:$san,"; done
-    eval "openssl req -nodes -subj /CN=${1}/ $KEY_SRC \
+    eval "openssl req -nodes -subj /CN=${1}/ ${KEY_SPEC} \
       -reqexts SAN -extensions SAN \
       -config <(printf \"[req]\ndistinguished_name=rdn\n[rdn]\n[SAN]\n`echo ${s} | sed 's/.$//'`\")"
   fi
@@ -111,14 +143,13 @@ elif [ $SIGN_TYPE = "Self-signed" ]; then
   all_args="${@}"
 
   # For self-signed there is ONLY "CN=blah", and NO SubjAltNames.
-  #openssl req -new -newkey ${KEY_SPEC} -days 3650 \
-  #  -nodes -x509 -subj /CN="${all_args}"/ -keyout /dev/stdout 2>/dev/null
-  eval "openssl req -nodes -days 3650 -x509 -subj /CN="${all_args}"/ ${KEY_SRC}"
+  eval "openssl req -nodes -days 3650 -x509 -subj '/CN=${all_args}/' ${KEY_SPEC}"
 else
   echo "Not implemented yet"
 fi
 
-# Clean up EC params
+
+# Clean up temporary EC params file
 if [ -n "$EC_PARAMS" ]; then
-  rm  "${EC_PARAMS}"
+  rm "${EC_PARAMS}"
 fi
